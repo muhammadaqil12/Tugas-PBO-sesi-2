61544f08150f16c2cbd47520ff08b76e5a6811ce
muhammadaqil12 <115065268+muhammadaqil12@users.noreply.github.com>
2024-03-29 18:32:24 +0700

    Create persegi.java

---
+++ persegi.java
@@ -0,0 +1,26 @@
+public class Persegi {
+    private double sisi;
+    private double luas;
+    private double keliling;
+
+    public Persegi (double sisi) {
+        this.sisi = sisi;
+        System.out.println("Sisi perseginya adalah: "+ sisi);
+    }
+
+    public void setSisi (double sisi) {
+        this.sisi = sisi;
+        System.out.println("Sisi perseginnya adalah: "+ sisi);
+    }
+
+    public double getLuas(){
+        luas = sisi * sisi;
+        return luas;
+    }
+
+    public double getKeliling(){
+        keliling = sisi * 4;
+        return keliling;
+    }
+
+}
